0ab900b6cf6ac60e135a4c7f01b66dcdbc78a290
Oz <fraelfo@fastwebnet.it>
2014-09-13 16:02:17 +0200

    Update to 1.4.7

--- ShestakUI_Config/ShestakUI_Config.lua
+++ ShestakUI_Config/ShestakUI_Config.lua
@@ -692,7 +692,7 @@ function CreateUIConfig()
 
 	local TitleBoxVerText = TitleBoxVer:CreateFontString("UIConfigTitleVer", "OVERLAY", "GameFontNormal")
 	TitleBoxVerText:SetPoint("CENTER")
-	TitleBoxVerText:SetText("ShestakUI "..T.version.." (OzEdit 1.4.6)")
+	TitleBoxVerText:SetText("ShestakUI "..T.version.." (OzEdit 1.4.7)")
 
 	-- Main Frame Title
 	local TitleBox = CreateFrame("Frame", "TitleBox", UIConfigMain)
